bcbbd57feadfa44d1776f0d700a1a1946d387b65
LucianoCarvalhoJunior <lucianotirust@outlook.com>
2023-12-01 19:04:16 -0300

    avatar

--- app/src/common/components/MobileNav.tsx
+++ app/src/common/components/MobileNav.tsx
@@ -18,6 +18,7 @@ import { useContext } from "react";
 import { FiChevronDown, FiMenu } from "react-icons/fi";
 import { UserRoleColor } from "../../auth/contants/roles";
 import { AuthContext } from "../../auth/contexts/AuthContext";
+import { UserRole } from "../../auth/types";
 
 interface MobileProps extends FlexProps {
   onOpen: () => void;
@@ -66,9 +67,9 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
               <HStack>
                 <Avatar
                   size={"sm"}
-                  src={
-                    "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-                  }
+                  bg={UserRoleColor[user?.category?.role as UserRole]}
+                  color="white"
+                  name={user?.name}
                 />
                 <VStack
                   display={{ base: "none", md: "flex" }}
