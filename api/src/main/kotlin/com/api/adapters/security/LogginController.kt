bd1f50e72645477dd2e77c31bec26a32a38b20f0
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2023-12-02 01:48:15 -0300

    esqueci uma virgula...

--- api/src/main/kotlin/com/api/adapters/security/LogginController.kt
+++ api/src/main/kotlin/com/api/adapters/security/LogginController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class LogginController (
 	private val userService: UserService,
-	private val encoderPassword: EncoderPassword
+	private val encoderPassword: EncoderPassword,
 	private val jwtUtil: JWTUtil
 ) {
 	@PostMapping("/auth/login")
