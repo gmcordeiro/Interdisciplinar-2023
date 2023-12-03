8a72c19056bf3f0989dd8ecd5d7a6f398ddf8b7d
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2023-12-03 17:04:44 -0300

    Correção da serialização do userquery

--- api/src/main/kotlin/com/api/application/user/UserQuery.kt
+++ api/src/main/kotlin/com/api/application/user/UserQuery.kt
@@ -7,12 +7,30 @@ data class UserQuery(
     val id: Long?,
     val name: String,
     val email: String,
-    val category: UserCategory
+    val category: UserCategory,
+	val rg: String,
+	val cpf: String,
+	val telefone: String,
+	val father: String,
+	val mother: String,
+	val academic: Boolean,
+	val ra: String,
+	val curse: String,
+	val period: String
 )
 
 fun UserQuery.fromUser(user: User) = UserQuery(
 	id = id,
 	name = name,
 	email = email,
-	category = category
+	category = category,
+	rg = rg,
+	cpf = cpf,
+	telefone = telefone,
+	father = father,
+	mother = mother,
+	academic = academic,
+	ra = ra,
+	curse = curse,
+	period = period
 )
