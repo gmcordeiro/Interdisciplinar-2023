196ecdc58c4df1ad213960743f0bf72fc4f933b3
Guilherme M. Cordeiro <guicordeiro2009@gmail.com>
2023-12-03 15:51:03 -0300

    Adição de campos no user command

--- api/src/main/kotlin/com/api/application/user/UserCommand.kt
+++ api/src/main/kotlin/com/api/application/user/UserCommand.kt
@@ -7,28 +7,64 @@ data class UserCommand(
 	val name: String,
 	val email: String,
 	var category: UserCategory,
-	var password: String
+	var password: String,
+	val rg: String,
+	val cpf: String,
+	val telefone: String,
+	val father: String,
+	val mother: String,
+	val academic: Boolean,
+	val ra: String,
+	val curse: String,
+	val period: String
 )
 
 data class UserRequest(
 	val name: String,
 	val email: String,
 	var category: Long,
-	var password: String
+	var password: String,
+	val rg: String,
+	val cpf: String,
+	val telefone: String,
+	val father: String,
+	val mother: String,
+	val academic: Boolean,
+	val ra: String,
+	val curse: String,
+	val period: String
 )
 
 fun UserRequest.toCommand(category: UserCategory) = UserCommand(
 	name = name,
 	email = email,
 	category = category,
-	password = password
+	password = password,
+	rg = rg,
+	cpf = cpf,
+	telefone = telefone,
+	father = father,
+	mother = mother,
+	academic = academic,
+	ra = ra,
+	curse = curse,
+	period = period
 )
 
 fun UserCommand.toUser() = User (
 	name = name,
 	email = email,
 	category = category,
-	password = password
+	password = password,
+	rg = rg,
+	cpf = cpf,
+	telefone = telefone,
+	father = father,
+	mother = mother,
+	academic = academic,
+	ra = ra,
+	curse = curse,
+	period = period
 )
 
 fun UserCommand.toUser(userID: Long?) = User (
@@ -36,5 +72,14 @@ fun UserCommand.toUser(userID: Long?) = User (
 	name = name,
 	email = email,
 	category = category,
-	password = password
+	password = password,
+	rg = rg,
+	cpf = cpf,
+	telefone = telefone,
+	father = father,
+	mother = mother,
+	academic = academic,
+	ra = ra,
+	curse = curse,
+	period = period
 )
